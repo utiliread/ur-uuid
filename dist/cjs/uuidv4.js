b52c3e193dda40a6a793e30d5e919cef7f473025
Rasmus Melchior Jacobsen <rmja@laesoe.org>
2018-03-06 16:52:36 +0100

    chore(all) target es5

--- dist/cjs/uuidv4.js
+++ dist/cjs/uuidv4.js
@@ -1,9 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 function uuidv4() {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
-        const r = Math.random() * 16 | 0;
-        const n = c === 'x' ? r : (0x8 | (r & 0x3));
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var r = Math.random() * 16 | 0;
+        var n = c === 'x' ? r : (0x8 | (r & 0x3));
         return n.toString(16);
     });
 }
--- dist/esm/uuidv4.js
+++ dist/esm/uuidv4.js
@@ -1,7 +1,7 @@
 export function uuidv4() {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
-        const r = Math.random() * 16 | 0;
-        const n = c === 'x' ? r : (0x8 | (r & 0x3));
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var r = Math.random() * 16 | 0;
+        var n = c === 'x' ? r : (0x8 | (r & 0x3));
         return n.toString(16);
     });
 }
